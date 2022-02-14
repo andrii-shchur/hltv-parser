1913c6e4af2d48e3f774bf92d63b60cf4890c26a
Andrii <sparrow.andriy@gmail.com>
2022-02-14 20:45:01 +0200

    added webclient

---
+++ WebClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using HtmlAgilityPack;
+using ScrapySharp.Network;
+
+namespace CW
+{
+    public class WebClient
+    {
+        private static WebClient _client;
+        private ScrapingBrowser _browser;
+        
+        private WebClient(){}
+
+        public static WebClient GetInstance()
+        {
+            if (_client == null)
+            {
+                _client = new WebClient();
+                _client._browser = new ScrapingBrowser();
+            }
+
+            return _client;
+        }
+        
+        public HtmlNode GetPage(string url)
+        {
+            var pageResult = _browser.NavigateToPage(new Uri(url));
+            var page = pageResult.Html;
+            
+            return page;
+        }
+    }
+}
